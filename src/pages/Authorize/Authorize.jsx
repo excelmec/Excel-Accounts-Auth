3513be20884788a96461df012bbc050bd8116abf
Srividya Krishnakumar <srividya.krishnakumar99@gmail.com>
2020-12-24 06:56:16 +0000

    Log OAuth errors.

--- src/pages/Authorize/Authorize.jsx
+++ src/pages/Authorize/Authorize.jsx
@@ -1,23 +1,34 @@
 import React, { useEffect } from 'react';
 
-const authorizedSites = [
-  'http://localhost',
-  'http://localhost:3000',
-  'http://localhost:5000',
-  'https://staging.alfred.excelmec.org',
-  'https://alfred.excelmec.org',
-  'https://excelmec.org',
-  'https://staging.play.excelmec.org',
-  'https://play.excelmec.org',
-  'https://excel2020.netlify.app'
-]
+// const authorizedSites = new Set([
+//   'http://localhost',
+//   'http://localhost:3000',
+//   'http://localhost:5000',
+//   'http://127.0.0.1:8080',
+//   'https://staging.alfred.excelmec.org',
+//   'https://alfred.excelmec.org',
+//   'https://excelmec.org',
+//   'https://staging.play.excelmec.org',
+//   'https://play.excelmec.org',
+//   'https://excel2020.netlify.app'
+// ])
 
 const Authorize = () => {
   useEffect(() => {
+    // console.log('Parent hostname', window.parent.location.hostname)
     window.parent.postMessage(localStorage.getItem('refreshToken'), '*');
-    //window.parent.postMessage(localStorage.getItem('refreshToken'), authorizedSites.join(', '))
+    // const url = (window.location != window.parent.location)
+    //   ? document.referrer
+    //   : document.location.hostname;
+    // authorizedSites.forEach(authorizedSite => {
+    //   window.parent.postMessage(localStorage.getItem('refreshToken'), authorizedSite)
+    // })
+    // if (url in authorizedSites) {
+    //   window.parent.postMessage(localStorage.getItem('refreshToken'), url);
+    // }
+
   }, []);
-  return <h1>Hi</h1>;
+  return <h1 className='white'>Hi</h1>;
 };
 
 export default Authorize;
