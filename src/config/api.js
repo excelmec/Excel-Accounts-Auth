0812bea7ec6de8d899de7ce13a0c398b9ef701a7
afnannavaz <afnannavaz@gmail.com>
2021-11-07 09:47:06 +0530

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,7 @@
-const hostname = window.location.hostname === 'localhost' ? "staging.accounts.excelmec.org" : window.location.hostname;
+//const hostname = window.location.hostname === 'localhost' ? "staging.accounts.excelmec.org" : window.location.hostname;
+//const hostname = window.location.hostname === 'localhost' ? "excel-accounts-backend-ovhv32pszq-el.a.run.app" : window.location.hostname;
+const hostname = window.location.hostname === 'localhost' ? "localhost:5000" : window.location.hostname;
 
 export const WSRoot = `ws://${hostname}`;
-export const ApiRoot = `https://${hostname}/api`;
+// export const ApiRoot = `https://${hostname}/api`;
+export const ApiRoot = `https://${hostname}`;
--- src/config/oauth_config.js
+++ src/config/oauth_config.js
@@ -3,14 +3,18 @@
 const config = () => {
     if (process.env.NODE_ENV === 'development') {
         return {
-            clientId: '357322315568-stm2tq6ohqcdpivksv9dl9oa3mjh6b7m.apps.googleusercontent.com',
-            domain: 'ajeshkumar.eu.auth0.com',
+            //clientId: '357322315568-stm2tq6ohqcdpivksv9dl9oa3mjh6b7m.apps.googleusercontent.com',
+            clientId: '400055254952-f3tit3vs62vubio68pa1gctp6u1nf9ab.apps.googleusercontent.com',
+            // domain: 'ajeshkumar.eu.auth0.com',
+            domain: 'dev-rkk793px.auth0.com',
             redirectUrl: '/auth/login'
         };
     } else {
         return {
-            clientId: '357322315568-stm2tq6ohqcdpivksv9dl9oa3mjh6b7m.apps.googleusercontent.com',
-            domain: 'ajeshkumar.eu.auth0.com',
+            //clientId: '357322315568-stm2tq6ohqcdpivksv9dl9oa3mjh6b7m.apps.googleusercontent.com',
+            clientId: '400055254952-f3tit3vs62vubio68pa1gctp6u1nf9ab.apps.googleusercontent.com',
+            // domain: 'ajeshkumar.eu.auth0.com',
+            domain: 'dev-rkk793px.auth0.com',
             redirectUrl: '/auth/login'
         };
     }
