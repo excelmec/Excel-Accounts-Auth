40b5af10c82ed19fc4a2e68b8f697f3eef23e4ab
Ashish <ashishmp0471@gmail.com>
2023-01-28 13:42:55 +0530

    Resolved logout issue

--- src/pages/Logout/Logout.js
+++ src/pages/Logout/Logout.js
@@ -6,34 +6,15 @@ import configs from '../../config/oauth_config';
 const config = configs();
 
 const Logout = () => {
-  const onFailure = (e) => {
-    console.log('Error logging out: ', e);
-  }
-  const onLogoutSuccess = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const redirectUrl = urlParams.get('redirect_to');
-    console.log('Successfully logged out!');
-    window.location.href = redirectUrl;
-  }
-
-  const { signOut/*, loaded */ } = useGoogleLogout({
-    onFailure,
-    onLogoutSuccess,
-    clientId: config.clientId,
-  });
+  
 
   useEffect(() => {
     localStorage.clear();
     const cookies = document.cookie.split(';');
-
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i];
-      const eqPos = cookie.indexOf('=');
-      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
-    }
-
-    signOut();
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirectUrl = urlParams.get('redirect_to');
+    console.log('Successfully logged out!');
+    window.location.href = redirectUrl;
   });
 
   return (
