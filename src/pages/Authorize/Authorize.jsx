60fad316102578498297663e0a2f07fbcaa2c6b4
Abhinav <abhinavmohanan17@gmail.com>
2023-09-09 23:06:16 +0530

    Shared Login

--- src/pages/Authorize/Authorize.jsx
+++ src/pages/Authorize/Authorize.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect } from 'react';
-
-// const authorizedSites = new Set([
-//   'http://localhost',
-//   'http://localhost:3000',
-//   'http://localhost:5000',
-//   'http://127.0.0.1:8080',
-//   'https://staging.alfred.excelmec.org',
-//   'https://alfred.excelmec.org',
-//   'https://excelmec.org',
-//   'https://staging.play.excelmec.org',
-//   'https://play.excelmec.org',
-//   'https://excel2020.netlify.app'
-// ])
+import React, { useEffect } from "react";
+import { authorizedSites } from "../../config/authorized";
 
 const Authorize = () => {
   useEffect(() => {
-    // console.log('Parent hostname', window.parent.location.hostname)
-    window.parent.postMessage(localStorage.getItem('refreshToken'), '*');
-    // const url = (window.location != window.parent.location)
-    //   ? document.referrer
-    //   : document.location.hostname;
-    // authorizedSites.forEach(authorizedSite => {
-    //   window.parent.postMessage(localStorage.getItem('refreshToken'), authorizedSite)
-    // })
-    // if (url in authorizedSites) {
-    //   window.parent.postMessage(localStorage.getItem('refreshToken'), url);
-    // }
+    window.onmessage = function (e) {
+      var parent = window.parent;
+      var url =
+        window.location !== window.parent.location
+          ? document.referrer
+          : document.location.href;
 
+      //Prevent Unauthorized access
+      if (!authorizedSites.has(url)) {
+        console.log("Unauthorized access from " + url);
+        return;
+      }
+
+      var data = {
+        origin: e.origin,
+      };
+      data.isLoggedin = false;
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (refreshToken) {
+        data.refreshToken = refreshToken;
+        data.isLoggedin = true;
+      }
+
+      parent.postMessage(JSON.stringify(data), "*");
+    };
   }, []);
-  return <h1 className='white'>Hi</h1>;
+  return <></>;
 };
 
 export default Authorize;
