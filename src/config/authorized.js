26704d63ab098c62387e1c2a115a91ae5cb87d49
Abhinav <abhinavmohanan17@gmail.com>
2023-09-09 22:17:28 +0530

    Shared Login

---
+++ src/config/authorized.js
@@ -0,0 +1,9 @@
+//Domains authorized to access accounts
+
+export const authorizedSites = new Set([
+    "http://localhost",
+    "http://localhost:3000/",
+    "http://localhost:5173/",
+    "https://alfred.excelmec.org/",
+]);
+
